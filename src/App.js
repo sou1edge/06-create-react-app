6ec3d06f625cf2df134c8473f779cbd4cb0c9654
sou1edge <m8167085@gmail.com>
2023-04-15 12:41:26 -0400

    Add tag h1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from Create React App</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!!!
+          Learn React
         </a>
       </header>
     </div>
