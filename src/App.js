619263e4481d1881d6533e24a94409bd1df4df6c
sou1edge <m8167085@gmail.com>
2023-04-15 12:46:07 -0400

    comment tag p

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from Create React App</h1>
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         <a
           className="App-link"
           href="https://reactjs.org"
